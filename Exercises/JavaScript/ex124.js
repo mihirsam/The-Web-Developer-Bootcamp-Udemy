2e32fbd193868ac66ba6388a5fbea55352209aa3
mihirsam <xenon.of.x@gmail.com>
2018-04-20 06:14:44 +0530

    completed todoList

---
+++ Exercises/JavaScript/ex124.js
@@ -0,0 +1,44 @@
+var todoList = []
+while(true)
+{
+  var choice = prompt("Enter The Choice");
+  if(choice == "new")
+  {
+    var newEvent = prompt("What You Want To Do ?");
+    todoList.push(newEvent);
+    console.log(newEvent + " Has Been Added To The List.")
+  }
+
+  else if(choice == "list")
+  {
+    console.log("\n\nTO DO List : ")
+    for(var i=0; i < todoList.length; i++)
+    {
+        console.log("["+i+"] : "+todoList[i]);
+    }
+  }
+
+  else if (choice == "delete")
+  {
+    var index = prompt("Enter The Index To Delete The Event")
+    if(index < todoList.length)
+    {
+      console.log("\n\n"+todoList[index] + " Has Been Removed From The List.")
+      todoList.splice(index, 1)
+    }
+    else
+    {
+      console.log("Invalid Index")
+    }
+
+  }
+  else if (choice == "exit")
+  {
+    console.log("\n\nBye!")
+    break;
+  }
+  else
+  {
+    console.log("Invalid Input");
+  }
+}
