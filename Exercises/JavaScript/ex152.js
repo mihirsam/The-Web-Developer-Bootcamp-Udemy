1fc9da4bd28bade6030b0885122ef24af0a9af75
mihirsam <xenon.of.x@gmail.com>
2018-05-07 13:15:34 +0530

    minor changes

--- Exercises/JavaScript/ex152.js
+++ Exercises/JavaScript/ex152.js
@@ -3,25 +3,22 @@ var flag = 0, blinkCount = 0, imgCount = 3;
 
 var colors = ["#0af5fa", "#0ab1fa", "#0a3efa", "#560afa", "#b70afa", "#fa0ad9", "#fa0a65", "#fa0a1e", "#fa5e0a", "#facc0a", "#cbfa0a", "#69fa0a", "#0afa1b", "#0afa77", "#0afae5", "#0a8efa"];
 
-setInterval(blink, 100);
-
 function blink()
 {
   if(blinkCount==15)
   {
     blinkCount = 0;
+    blink();
   }
 
   else
   {
     var h1 = document.querySelectorAll("h1");
-    var body = document.querySelector("body");
     var img = document.querySelectorAll("img");
 
     h1[0].style.color = colors[blinkCount];
     h1[1].style.color = colors[blinkCount];
     img[imgCount].style.background = colors[blinkCount];
-    body.style.background = colors[15 - blinkCount];
     blinkCount++;
   }
 }
@@ -46,7 +43,7 @@ function secondClick()
     h1[1].textContent = "You Choose Charmander";
     flag++;
     imgCount = 1;
-    setInterval(blink, 500);
+    setInterval(blink, 200);
   }
 }
 
@@ -62,6 +59,7 @@ function thirdClick()
   }
 }
 
+setInterval(blink, 100);
 var img = document.querySelectorAll("img");
 img[0].addEventListener("click", firstClick);
 img[1].addEventListener("click", secondClick);
