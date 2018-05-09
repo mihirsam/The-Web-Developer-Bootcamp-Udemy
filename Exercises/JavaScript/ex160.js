f131740c4e44cb20e8321dda063134b1a912a770
mihirsam <xenon.of.x@gmail.com>
2018-05-09 23:11:32 +0530

    generate random colors

--- Exercises/JavaScript/ex160.js
+++ Exercises/JavaScript/ex160.js
@@ -1,7 +1,32 @@
-var colors = ["rgb(250, 250, 0)", "rgb(250, 250, 250)", "rgb(20, 250, 0)", "rgb(150, 150, 100)", "rgb(100, 150, 220)", "rgb(50, 50, 150)", "rgb(2,20,100)", "rgb(250, 0, 250)", "rgb(250, 0, 100)","rgb(250, 250, 0)", "rgb(0,0,0)"];
+function getRandomColor()
+{
+  var arr = [];
+  var r=0, g=0, b=0;
+  var temp;
 
+  for(var i=0; i<6; i++)
+  {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+
+    arr[i] = "rgb("+r+","+g+","+b+")";
+  }
+
+  return arr;
+}
+
+function pickColor()
+{
+  var num = Math.floor(Math.random() * colors.length);
+  return num;
+}
+
+
+var colors = getRandomColor();
 var colorTile = document.querySelectorAll(".colorTile");
-var pickedColor = colors[5];
+var num = pickColor();
+var pickedColor = colors[num];
 var rgbDisplay = document.querySelector("#rgbDisplay");
 var h1 = document.querySelectorAll("h1");
 
